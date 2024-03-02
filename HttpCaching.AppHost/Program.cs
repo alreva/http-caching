3da11454bf2e18809faa4ecdbbc4d4d1c9aa24aa
Alex Reva <alreva@gmail.com>
2024-03-02 16:31:24 +0200

    initial checkin

---
+++ HttpCaching.AppHost/Program.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.HttpCaching_ApiService>("apiservice");
+
+builder.AddProject<Projects.HttpCaching_Web>("webfrontend")
+    .WithReference(apiService);
+
+builder.Build().Run();
